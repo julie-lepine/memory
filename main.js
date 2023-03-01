b7a1e412f78bf365fd4c8b64d52bf8828bd60fab
Julie lepine <lepine.julie@live.fr>
2023-03-01 15:50:19 +0100

    3rd commit : a little bit of style

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// déclarer un tableau de toutes les cartes et créer de l'aléatoire pour les placer 
+
 let jeuTableau;
 let allCards = document.querySelectorAll('.card')
 let cptClickCurrent = 0
@@ -46,5 +48,9 @@ function playGame(card) {
         cptClickCurrent = 0
         // on oublie les cartes précédemment retournées car on ne les recherchera pas une nouvelle fois au tour d'après :
         dataImageShowed = ""
+
+        // compter les crds qui n'ont pas la classe finded
+        // si = 0 alors on a gagné le jeu
+
     }
 }
